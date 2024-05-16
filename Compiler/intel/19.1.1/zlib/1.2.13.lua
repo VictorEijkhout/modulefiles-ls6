76490f75c1c3a279f34fa2881d7fe5e5a42ccd1b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-05-16 08:22:30 -0500

    remove autostuff under compiler

--- Compiler/intel/19.1.1/zlib/1.2.13.lua
+++ Compiler/intel/19.1.1/zlib/1.2.13.lua
@@ -1,8 +1,17 @@
-prepend_path( "LD_LIBRARY_PATH",pathJoin("/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel","lib")   )
-setenv( "LMOD_ZLIB_DIR",     "/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel"                     )
-setenv( "LMOD_ZLIB_INC",     pathJoin("/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel","include")   )
-setenv( "LMOD_ZLIB_LIB",     pathJoin("/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel","lib")   )
-setenv( "TACC_ZLIB_DIR",     "/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel"                     )
-setenv( "TACC_ZLIB_INC",     pathJoin("/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel","include")   )
-setenv( "TACC_ZLIB_LIB",     pathJoin("/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel","lib")   )
-prepend_path( "PKG_CONFIG_PATH", pathJoin( "/work/00434/eijkhout/zlib/installation-1.2.13-milan-intel","share/pkgconfig" ) )
+local helpMsg = [[
+..
+Package: zlib/1.2.13
+..
+The zlib modulefile defines the following variables:
+TACC_ZLIB_DIR, TACC_ZLIB_LIB, TACC_ZLIB_INC
+]]
+help(helpMsg)
+setenv( "TACC_ZLIB_DIR",     "/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19" )
+setenv( "LMOD_ZLIB_DIR",     "/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19" )
+setenv( "TACC_ZLIB_INC",     pathJoin("/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19","include")   )
+setenv( "LMOD_ZLIB_INC",     pathJoin("/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19","include")   )
+setenv( "TACC_ZLIB_LIB",     pathJoin("/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19","lib")   )
+setenv( "LMOD_ZLIB_LIB",     pathJoin("/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19","lib")   )
+prepend_path( "LD_LIBRARY_PATH",pathJoin("/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19","lib")   )
+prepend_path( "PKG_CONFIG_PATH", 
+                    pathJoin( "/work/00434/eijkhout/zlib/installation-zlib-1.2.13-milan-intel19","share/pkgconfig" ) )
